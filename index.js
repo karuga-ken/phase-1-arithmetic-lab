b4e638d5661839f70b41e8b34b0aa48397dd0049
Karuga Mbugua <karugambugua@icloud.com>
2023-10-03 20:49:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+    const num1 = 31;
+    const num2 = 2;
+    const multiply = num1 * num2;
+    const random = Math.floor(Math.random() * 100)
+    const num3 = 22;
+    const num4 = 9;
+    const div = num3 % num4; 
+    const max = Math.max(1, 5, 6, 8, 20);
